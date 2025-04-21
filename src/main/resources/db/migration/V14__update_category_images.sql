c6858a203412c744799cf999b6758ce91971907e
Rehab Qadhabi <rehabqadhabi@Rehabs-MacBook-Pro.local>
2025-04-21 13:57:16 +0300

    Updated image URLs for all menu items to use realistic category-specific photos

---
+++ src/main/resources/db/migration/V14__update_category_images.sql
@@ -0,0 +1,5 @@
+UPDATE menu SET image_url = 'https://images.unsplash.com/photo-1504754524776-8f4f37790ca0' WHERE category_id = 1;
+UPDATE menu SET image_url = 'https://www.mortons.ie/wp-content/uploads/2024/03/20230627-4E6A0689.jpg' WHERE category_id = 2;
+UPDATE menu SET image_url = 'https://www.southernliving.com/thmb/rHzEnavNr0nVqnUy52cwClYF8P8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/2678201_HIHAn_27538-2000-47842017c4a9440ea1c615cc941c42f2.jpg' WHERE category_id = 3;
+UPDATE menu SET image_url = 'https://int.japanesetaste.com/cdn/shop/articles/how-to-make-makizushi-sushi-rolls-japanese-taste.jpg?v=1707914944&width=2560' WHERE category_id = 4;
+UPDATE menu SET image_url = 'https://images.services.kitchenstories.io/q53BSWMP8q8OAq_wsoZBV-P-_Qs=/1920x0/filters:quality(85)/images.kitchenstories.io/wagtailOriginalImages/A1492-final-photo-38.jpg' WHERE category_id = 5;
