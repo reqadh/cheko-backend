42758db37dd705fa4f266a9a3ceb4f78c036bbc4
Rehab Qadhabi <rehabqadhabi@Rehabs-MacBook-Pro.local>
2025-04-24 09:25:59 +0300

    feat: update restaurant logo URLs in database
    - Added new Flyway migration to update logo_url for sample restaurants

---
+++ src/main/resources/db/migration/V18__update_restaurant_logos.sql
@@ -0,0 +1,17 @@
+
+
+UPDATE restaurants 
+SET logo_url = 'https://i.pinimg.com/736x/08/3b/2f/083b2fe2646cd064e3a294bb716810f9.jpg' 
+WHERE name = 'Kitchen Cafe';
+
+UPDATE restaurants 
+SET logo_url = 'https://scontent.fruh7-1.fna.fbcdn.net/v/t39.30808-6/469875015_553865330954114_5258407149995963867_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=gpdTPeips6YQ7kNvwG7WiR3&_nc_oc=AdmIqrUhATqf8GDxpc5aM-TtOBVgjZUwdlLEyk2o-P_cwW_awWalhXG3SGVCn9YhL3o&_nc_zt=23&_nc_ht=scontent.fruh7-1.fna&_nc_gid=rQu_N6Fn1nL20uAxUH4oWg&oh=00_AfEYPBBzY1XAEROJWpDSBaieMkIKc5yIQJC9TMFT2DQzsA&oe=680FAB3C' 
+WHERE name = 'Golden Spoon';
+
+UPDATE restaurants 
+SET logo_url = 'https://images-platform.99static.com/OigWRJqZBj230L_or0uKdVZoU0M=/200x200:1800x1800/500x500/top/smart/99designs-contests-attachments/148/148510/attachment_148510653' 
+WHERE name = 'Tasty Bites';
+
+UPDATE restaurants 
+SET logo_url = 'https://scontent.fruh7-1.fna.fbcdn.net/v/t39.30808-6/301837347_388762286793079_9138408054128403821_n.png?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=SQrZL-2-Ju8Q7kNvwEFFrjf&_nc_oc=AdkyOlvTfM1Sb7QH3Rz-iBmBNtKBxGpbjyWpzUBfbFdhVaPK0mnQtDPK0fFUwDXDXMc&_nc_zt=23&_nc_ht=scontent.fruh7-1.fna&_nc_gid=Zpz4o9wkP6L9QFgFuRLn6Q&oh=00_AfFcfpxXG67-5KRuWdLLABj7HWNi19S8GV0dw4gbfKquVQ&oe=680F94DA' 
+WHERE name = 'Riyadh Delight';
